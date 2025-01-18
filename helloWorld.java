64f2b5e08065c3f6da03839f18c6a805589e54ff
henrietta <hetty8004@gmail.com>
2025-01-18 03:35:06 -0800

    task completed

--- helloWorld.java
+++ helloWorld.java
@@ -2,7 +2,6 @@ class helloWorld{
     public static void main(String[] args) {
     System.out.println("Hello World");
     System.out.println("I'm starting 2025 on a new note");
-    System.out.println("Learning Java");
 
     }
 }
